d2fb0b78f3e7544ad4694e626966a281695fd09a
shubhapatil154 <53892562+shubhapatil154@users.noreply.github.com>
2019-08-24 10:24:22 +0530

    Create kthele_week2a.c

---
+++ kthele_week2a.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
+int sort_search(int a[],int n,int k)
+{
+	int i,j,min;
+	for(i=0;i<k;i++)
+	{
+		min=i;
+		for(j=i+1;j<n-1;j++)
+		{
+			if(a[j]<a[min])
+				min=j;
+		}
+		swap(&a[i],&a[min]);
+	}
+	return a[k-1];
+}
+
+
+int main()
+{
+	int arr[100];
+	int num,i;
+	int k,k_thele;
+	printf("ENTER THE NUMBER OF ELEMENTS IN AN ARRAY\n");
+	scanf("%d",&num);
+	printf("ENTER THE ARRAY ELEMENTS\n");
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("ENTER  K\n");
+	scanf("%d",&k);
+	k_thele=sort_search(arr,num,k);
+	printf("K_TH SMALLEST ELEMENT IS %d\n",k_thele);
+}
