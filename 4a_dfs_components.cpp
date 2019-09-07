97eda458f822c1c9190a40f985af90280742ffa2
shubhapatil154 <53892562+shubhapatil154@users.noreply.github.com>
2019-09-07 10:07:20 +0530

    Create 4a_dfs_components.cpp

---
+++ 4a_dfs_components.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class connected
+{
+int adjacent[10][10],visited[10],v,n;
+public:
+	void getdata()
+	{
+		cout<<"ENTER THE NUMBER OF COMPONENTS IN YOUR GRAPH"<<endl;
+		cin>>n;
+		for (int i=0;i<n;i++)
+		{
+			visited[i]=0;
+		}
+		cout<<"ENTER THE ADJACENCY MATRIX OF THE GRAPH"<<endl;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				cin>>adjacent[i][j];
+			}
+		}
+	}
+
+void dfs(int v)
+{
+	cout<<v+1<<",";
+	visited[v]=1;
+	for(int i=0;i<n;i++)
+	{
+		if(adjacent[v][i]==1 && visited[i]==0)
+		{
+			dfs(i);
+		}
+	}
+}
+void node_visited()
+{
+	for(int j=0;j<n;j++)
+	{
+		if(visited[j]==0)
+		{
+			dfs(j);
+			cout<<"\n";
+		}
+	}
+}
+};
+int main(){
+    connected c;
+    c.getdata();
+    c.node_visited();
+    return 0;
+
+}
